6c673fdd2adbe41072ecc190e7a7b07a2fa62860
aebibtech <paul@aebibtech.com>
2026-05-16 02:11:45 +0800

    feat: update Auth0 configuration to use environment variables and improve server URL handling

--- landing/auth.config.mjs
+++ landing/auth.config.mjs
@@ -2,15 +2,19 @@ import Auth0 from "@auth/core/providers/auth0";
 import { defineConfig } from "auth-astro";
 
 export default defineConfig({
+    secret: process.env.AUTH_SECRET,
 	providers: [
 		Auth0({
-			clientId: import.meta.env.AUTH_CLIENT_ID,
-			clientSecret: import.meta.env.AUTH_CLIENT_SECRET,
-			issuer: import.meta.env.AUTH_ISSUER,
+			clientId: process.env.AUTH_CLIENT_ID,
+			clientSecret: process.env.AUTH_CLIENT_SECRET,
+			issuer: process.env.AUTH_ISSUER ? (() => {
+				let url = process.env.AUTH_ISSUER.startsWith('http') ? process.env.AUTH_ISSUER : `https://${process.env.AUTH_ISSUER}`;
+				return url.endsWith('/') ? url : `${url}/`;
+			})() : undefined,
             authorization: { 
                 params: { 
                     scope: "openid profile email", 
-                    audience: import.meta.env.AUTH_AUDIENCE 
+                    audience: process.env.AUTH_AUDIENCE 
                 } 
             },
 		}),
